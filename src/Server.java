9e215669621831655d2c015d6bd0ca26e2eb92cf
Essid Belhassen <essidbelhassan@gmail.com>
2021-12-11 10:18:14 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,7 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Locale;
 
 public class Server extends UnicastRemoteObject implements MyInterface {
 
@@ -13,5 +14,47 @@ public class Server extends UnicastRemoteObject implements MyInterface {
         java.rmi.registry.LocateRegistry.createRegistry(1250);
 
         Naming.rebind("rmi://127.0.0.1:1250/server", server);
+    }
+    
+    @Override
+    public String  reverse(String ch)
+    {
+        return new StringBuilder(ch).reverse().toString();
+    }
+    
+    @Override
+    public char minChar(String ch)
+    {
+        char c =ch.charAt(0);
+        for (int i=1; i<ch.length(); i++)
+        {
+            if(ch.charAt(i) < c)
+            {
+                c=ch.charAt(i);
+            }
+        }
+        return c;
+    }
+    
+    @Override
+    public String upperLower(String ch)
+    {
+        StringBuilder rt = new StringBuilder();
+        for (int i=0; i<ch.length(); i++)
+        {
+            if(ch.charAt(i)>='a' && ch.charAt(i)<='z')
+            {
+                rt.append((char)(ch.charAt(i)-32));
+            }
+            else if(ch.charAt(i)>='A' && ch.charAt(i)<='Z')
+            {
+                rt.append((char)(ch.charAt(i)+32));
+            }
+            else
+            {
+               rt.append(s.charAt(i));
+            }
+        }
+        return rt.toString();
     }
 }
