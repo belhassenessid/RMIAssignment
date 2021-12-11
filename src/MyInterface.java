ee31d6522a29ee5cf7ac9629f84c879bc3b72d30
Essid Belhassen <essidbelhassan@gmail.com>
2021-12-11 10:10:32 +0100

    Update MyInterface.java

--- src/MyInterface.java
+++ src/MyInterface.java
@@ -2,5 +2,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface MyInterface extends Remote {
-
+    public String reverse(String ch) throws RemoteException;
+    public char minChar(String ch) throws RemoteException;
+    public String upperLower(String ch) throws RemoteException;
 }
